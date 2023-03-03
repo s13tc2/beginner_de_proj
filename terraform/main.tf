4e18e323b7242c92b4b9f4b86b4e115ec94d0c4a
s13tc2 <s13tc2@protonmail.com>
2023-03-02 21:15:37 -0500

    edit main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -150,12 +150,14 @@ sudo chmod 666 /var/run/docker.sock
 sudo apt install make
 
 echo 'Clone git repo to EC2'
-cd /home/ubuntu && git clone https://github.com/s13tc2/beginner_de_proj.git && cd beginner_de_project && rm -rf env  && echo 'Setup Airflow environment variables'
+cd /home/ubuntu && git clone https://github.com/s13tc2/beginner_de_proj.git && cd beginner_de_project && make perms
+
+cd /home/ubuntu && cd beginner_de_project && sudo rm -rf env && 
 echo "
 AIRFLOW_CONN_POSTGRES_DEFAULT=postgres://airflow:airflow@localhost:5439/airflow
 AIRFLOW_CONN_AWS_DEFAULT=aws://?region_name=${var.aws_region}
 AIRFLOW_VAR_BUCKET=${aws_s3_bucket.sde-data-lake.id}
-" > env && make perms
+" > env
 
 
 echo 'Start Airflow containers'
